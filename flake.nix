50177ae5fc4489d6d54f94e3e16234bfb5412ca2
ProggerX <x@proggers.ru>
2026-06-15 17:40:56 +0300

    flake: update to fresh light architecture, add static build

--- flake.nix
+++ flake.nix
@@ -5,21 +5,24 @@
   };
   outputs = {
     flake-parts,
-    nixpkgs,
     self,
+    ...
   } @ inputs:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = nixpkgs.lib.platforms.unix;
-      perSystem = {pkgs, ...}: let
-        hpkgs = pkgs.haskellPackages;
-        opts = {
-          root = ./.;
-          source-overrides = {};
-        };
-        pkg = op': hpkgs.developPackage (opts // op');
-      in {
-        packages.default = pkg {};
-        devShells.default = pkg {
+    flake-parts.lib.mkFlake {inherit inputs;} (let
+      hpkgs = pkgs: pkgs.haskell.packages.ghc910;
+      opts = {
+        root = ./.;
+        source-overrides = {}; # Put overrides here
+      };
+      pkg = pkgs: op:
+        (hpkgs pkgs).developPackage (opts // op);
+    in {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
+      perSystem = {pkgs, ...}: {
+        formatter = pkgs.alejandra;
+        packages.default = pkg pkgs {};
+        packages.static = pkg pkgs.pkgsStatic {};
+        devShells.default = pkg pkgs {
           returnShellEnv = true;
           modifier = drv:
             pkgs.haskell.lib.addBuildTools drv (with pkgs; [
@@ -28,14 +31,11 @@
             ]);
         };
       };
-      flake = let
-        xPkgs = import nixpkgs {system = "x86_64-linux";};
-      in {
+      flake = {
         nixosModules.default = {pkgs, ...}: {
           imports = [./nixos];
           services.adhd.package = self.packages.${pkgs.stdenv.hostPlatform.system}.default;
         };
-        formatter.x86_64-linux = xPkgs.alejandra;
       };
-    };
+    });
 }
