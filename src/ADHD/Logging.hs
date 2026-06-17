4272fe5232050a02a7ce8ea5843cb4ef288e95f7
ProggerX <x@proggers.ru>
2026-06-17 16:25:52 +0300

    log: add flushing

--- src/ADHD/Logging.hs
+++ src/ADHD/Logging.hs
@@ -1,9 +1,11 @@
 module ADHD.Logging where
 
+import System.IO (hFlush, stdout)
+
 -- | Log level
 data Level = Info | Error
 
 -- | Log text with specified level
 log :: Level -> String -> IO ()
-log Info str = putStrLn $ "[INFO] " <> str
-log Error str = putStrLn $ "[ERROR] " <> str
+log Info str = putStrLn ("[INFO] " <> str) >> hFlush stdout
+log Error str = putStrLn ("[ERROR] " <> str) >> hFlush stdout
